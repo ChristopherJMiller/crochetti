001bb5521aab02f3cf0c0be154745f8ca2a05403
Christopher Miller <git@chrismiller.xyz>
2024-08-31 17:42:32 -0400

    update env

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# Tauri + React + Typescript
+# Crochetti
 
-This template should help get you started developing with Tauri, React and Typescript in Vite.
+## Getting Started
 
-## Recommended IDE Setup
+```sh
 
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+# Enter nix dev shell
+# Allow unfree needed for Android Studio and accept android sdk licenses
+NIXPKGS_ACCEPT_ANDROID_SDK_LICENSE=1 NIXPKGS_ALLOW_UNFREE=1 nix-shell
+
+# Install npm packages
+npm install
+
+# Build!
+npx tauri dev
+npx tauri build
+npx tauri android build
+```
--- default.nix
+++ default.nix
@@ -1,6 +1,30 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {} }:
   let
     overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+    
+    buildToolsVersion = "34.0.0";
+    cmakeVersion = "3.10.2";
+    androidComposition = pkgs.androidenv.composeAndroidPackages {
+      cmdLineToolsVersion = "8.0";
+      toolsVersion = "26.1.1";
+      platformToolsVersion = "35.0.1";
+      buildToolsVersions = [ buildToolsVersion ];
+      includeEmulator = true;
+      emulatorVersion = "35.1.4";
+      platformVersions = [ "34" ];
+      includeSources = false;
+      includeSystemImages = false;
+      systemImageTypes = [ "google_apis_playstore" ];
+      abiVersions = [ "armeabi-v7a" "arm64-v8a" "x86" "x86_64" ];
+      cmakeVersions = [ cmakeVersion ];
+      includeNDK = true;
+      ndkVersions = ["26.3.11579264"];
+      useGoogleAPIs = false;
+      useGoogleTVAddOns = false;
+      includeExtras = [
+        "extras;google;gcm"
+      ];
+    };
 in
   pkgs.mkShell rec {
     buildInputs = with pkgs; [ 
@@ -12,17 +36,26 @@ in
         wayland
         libxkbcommon
         nodejs_22
-        android-studio-full
+        androidComposition.androidsdk
+        libunwind
+        jdk
     ];
     RUSTC_VERSION = overrides.toolchain.channel;
 
-    LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+    LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ 
+      pkgs.llvmPackages_latest.libclang.lib 
+    ];
 
-    PROJECT_ROOT = builtins.toString ./.;
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+
+    ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+    NDK_HOME = "${ANDROID_HOME}/ndk-bundle";
+    GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_HOME}/build-tools/${buildToolsVersion}/aapt2";
 
     shellHook = ''
       export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
       export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+      export PATH="$(echo "$ANDROID_HOME/cmake/${cmakeVersion}".*/bin):$PATH"
       '';
     # Add precompiled library to rustc search path
     RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
